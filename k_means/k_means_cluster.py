770604017e1bc7b3856e0d5ca96c24885dc16c9e
helenzusa1 <helenzusa.g@gmail.com>
2017-11-05 17:57:36 -0800

    Add files via upload

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[5]:
 
 #!/usr/bin/python
 
@@ -17,25 +17,25 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat,targetFeatureSplit
 
-def Draw(pred,features,poi,mark_poi=False, name="image.png", f1_name="feaure 1", f2_name="feature 2"):
-    """some plotting code designed to help you visualize your clusters """
-    
-    ### plot each cluster with a different color-add more colors for
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+
+    """ some plotting code designed to help you visualize your clusters """
+    ### plot each cluster with a different color--add more colors for
     ### drawing more than five clusters
-    colors=["b","c","k","m","g"]
-    for ii,pp in enumarated(pred):
-        plt.scatter(features[ii][0],features[ii][1],color=colors[pred[ii]])
-        
-        ### if you like,place red stars over points that are POIs (just for funeis)
-        if mark_poi:
-            for ii,pp in enumarate(pred):
-                if poi[ii]:
-                    plt.scatter(features[ii][0],features[ii][1],color="r",marker="*")                                          
-        plt.xlabel(f1_name)
-        plt.ylabel(f2_name)
-        plt.savefig(name)
-        plt.show()
-                                         
+    colors = ["b", "c", "k", "m", "g"]
+
+    for ii, pp in enumerate(pred):
+        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+    ### if you like, place red stars over points that are POIs (just for funsies)
+
+    if mark_poi:
+        for ii, pp in enumerate(pred):
+            if poi[ii]:
+                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+    plt.xlabel(f1_name)
+    plt.ylabel(f2_name)
+    plt.savefig(name)
+    plt.show()                                         
                                          
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict=pickle.load(open("C:/ATT Training courses/ND Machine Learning/Projects/ud120-projects/final_project/final_project_dataset.pkl","r"))
@@ -48,7 +48,7 @@ feature_1="salary"
 feature_2="exercised_stock_options"    
 feature_3="total_payments"
 poi="poi"
-features_list=[poi,feature_1,feature_2,feature_3]
+features_list=[poi,feature_1,feature_2, feature_3]
 data=featureFormat(data_dict,features_list)
 poi,finance_features=targetFeatureSplit(data)
                                          
@@ -56,15 +56,16 @@ poi,finance_features=targetFeatureSplit(data)
 ### you'll want to change this line to
 ### for f1,f2,_in finance_features:
 ### *as it's currently written, the line below assume 2 features)
-for f1,f2,_ in finance_features:
+for f1,f2, _ in finance_features:
     plt.scatter(f1,f2)
 plt.show()
 
 ### cluster here: create predictions of the clutter labels
 ### for the data and store them to a list called pred
 clf_before=KMeans(n_clusters=2)
-pred_before = clf_before.fit_predict(finance_features)
-                                         
+clf_before.fit(finance_features)
+pred_before = clf_before.predict(finance_features)
+
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
@@ -80,7 +81,7 @@ print scaled_finance_features
 clf_after=KMeans(n_clusters=2)
 pred_after=clf_after.fit_predict(scaled_finance_features)
 
-for f1, f2,_ in scaled_finance_features:
+for f1, f2, _ in scaled_finance_features:
     plt.scatter(f1,f2)
 plt.show()
 
